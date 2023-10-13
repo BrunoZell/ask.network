452fcf881b7d398b0075a770f9dbf0c0387c6b1c
Bruno Zell <bruno@ask.network>
2023-10-13 17:24:16 +0200

    fix hydration error

--- contracts/app/components/AppBar.tsx
+++ contracts/app/components/AppBar.tsx
@@ -1,14 +1,21 @@
 import { FC } from 'react';
 import styles from '../styles/Home.module.css';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import dynamic from 'next/dynamic'
 import Image from 'next/image';
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+      (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+)
 
 export const AppBar: FC = () => {
   return (
     <div className={styles.AppHeader}>
       <Image src='/solanaLogo.png' height={30} width={200} />
       <span>Anchor Frontend Example</span>
-      <WalletMultiButton />
+      
+      <WalletMultiButtonDynamic />
     </div>
   );
 };

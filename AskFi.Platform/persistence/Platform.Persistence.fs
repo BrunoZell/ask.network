daae11f50b307a3a9bc9cdabb2965203c02e1873
Bruno Zell <ask@bruno.wtf>
2023-12-04 00:09:13 +0100

    t-parameterize cids

--- AskFi.Platform/persistence/Platform.Persistence.fs
+++ AskFi.Platform/persistence/Platform.Persistence.fs
@@ -4,26 +4,12 @@ open System.Runtime.CompilerServices
 
 // Todo: Make this an IPFS CID respecting multihash and multicodec
 [<IsReadOnly; Struct>]
-type ContentId = ContentId of Raw: byte array with
+type ContentId<'TDatum> = ContentId of Raw: byte array with
     static member Zero
         with get() = ContentId <| Array.zeroCreate<byte> 0
 
-
-// DataModel.KnowledgeBase.CapturedObservation [Sdk.Observation<'P>, 'P]
-// DataModel.KnowledgeBase.ObservationSequenceHead
-// DataModel.KnowledgeBase.ActionSequenceHead
-// DataModel.KnowledgeBase.ActionSequenceHead
-// DataModel.KnowledgeBase.ContextSequenceHead
-// DataModel.KnowledgeBase.ContextHistory
-// DataModel.KnowledgeBase
-// DataModel.UserSpace.QueryResultHead
-// DataModel.UserSpace.DecisionSequenceHead
-// DataModel.UserSpace.SimulationSequenceHead
-// DataModel.UserSpace.VisualizationSequenceHead
-// DataModel.UserSpace.ExecutionSequenceHead
-
 type IPlatformPersistence =
-    abstract member Cid<'TDatum> : 'TDatum -> ContentId
-    abstract member Load<'TDatum> : ContentId -> ValueTask<'TDatum>
-    abstract member Store<'TDatum> : 'TDatum -> ValueTask<ContentId>
-    abstract member Pin : ContentId -> ValueTask<bool>
+    abstract member Cid<'TDatum> : 'TDatum -> ContentId<'TDatum>
+    abstract member Load<'TDatum> : ContentId<'TDatum> -> ValueTask<'TDatum>
+    abstract member Store<'TDatum> : 'TDatum -> ValueTask<ContentId<'TDatum>>
+    abstract member Pin<'TDatum> : ContentId<'TDatum> -> ValueTask<bool>

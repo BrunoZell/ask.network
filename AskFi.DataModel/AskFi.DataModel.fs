b0491a10b9b09f1e8b3049c96e8261ab1edd9794
Bruno Zell <ask@bruno.wtf>
2023-12-04 00:09:22 +0100

    data model meta programming

--- AskFi.DataModel/AskFi.DataModel.fs
+++ AskFi.DataModel/AskFi.DataModel.fs
@@ -13,69 +13,51 @@ open System
 
 /// Generated immediately after an IObserver emitted a new observation grouping
 /// the observation with the latest local timestamp as of the runtime clock.
-type CapturedObservation = {
+type CapturedObservation<'ObservationSpace> = {
     /// Absolute timestamp of when this observation was recorded.
     /// As of runtime clock.
     At: DateTime
 
-    /// The 'Percept from Observation<'Percept> (type of the originating observer instance)
-    PerceptType: Type
-
     /// All percepts that appeared at this instant, as emitted by an IObserver<'Percept> instance.
-    Observation: ContentId // Sdk.Observation<'Percept>
+    Observation: ContentId<Sdk.Observation<'ObservationSpace>>
 }
 
 /// All captured observations within an observer group are sequenced into
 /// an observation sequence. Isolated observation sequences are a form of
 /// entry point for new information into the system. CIDs to such sequences
 /// are passed around to share information.
-type ObservationSequenceHead =
+type ObservationSequenceHead<'ObservationSpace> =
     | Identity of Nonce:uint64
-    | Observation of Node:ObservationSequenceNode
-and ObservationSequenceNode = {
+    | Observation of Node:ObservationSequenceNode<'ObservationSpace>
+and ObservationSequenceNode<'ObservationSpace> = {
     /// Links previous ObservationSequenceHead to form a temporal order.
-    Previous: ContentId // ObservationSequenceHead
+    Previous: ContentId<ObservationSequenceHead<'ObservationSpace>>
 
     /// Cid to the then latest CapturedObservation that caused this observation sequence head to be appended.
-    Capture: CapturedObservation
+    Capture: CapturedObservation<'ObservationSpace>
 }
 
 // #####################
 // ####  EXECUTION  ####
 // #####################
-//
+
 // Action execution traces are the other entry point of data flowing into the system,
 // which include the information gained from executing certain actions.
 // Keeping a handle on created action execution sequences therefore is important
-// if the data should not get lost.
-
-
-type ActionSet = {
-    /// All actions the strategy has decided to initiate.
-    Actions: Action array
-}
-and Action = {
-    /// 'Action to route to according IBroker<'Action>.
-    /// This type is taken from what the strategy emitted in its decision.
-    ActionType: Type
-
-    /// Cid to the action information. Has type of ActionType.
-    ActionCid: ContentId
-}
-
+// if the data should not get lost
 type ActionExecutionTrace<'Response> =
     /// Data emitted by the IBroker<'Action, 'Response> action execution.
     /// Responses could include an execution id, transaction, or validity proofs, but are abstracted out at the data structure level.
-    | Success of trace: 'Response'
+    | Success of trace: ContentId<'Response>
     /// IBroker action execution failed. This holds an exception message, if any, encountered during user code execution.
     | Error of ``exception``: string option
 
-type ActionExecutionResult = {
-    /// What action has been executed.
-    Executed: Action
+type ActionExecutionResult<'ActionSpace, 'Response> = {
+    /// What specific action has been executed by the IBroker
+    Executed: Sdk.Action<'ActionSpace>
 
     /// Trace output from broker.
-    Trace: ActionExecutionTrace
+    Trace: ActionExecutionTrace<'Response>
 
     /// When the used IBroker implementation started executing.
     InitiationTimestamp: DateTime
@@ -86,15 +68,15 @@ type ActionExecutionResult = {
 
 /// An action sequence is produced by a Broker Group, which forms the second type
 /// of data entry into the system, holding information we got from executing actions.
-type ActionSequenceHead =
+type ActionSequenceHead<'ActionSpace, 'Response> =
     | Identity of Nonce:uint64
-    | Action of Node:ActionSequenceNode
-and ActionSequenceNode = {
+    | Action of Node:ActionSequenceNode<'ActionSpace, 'Response>
+and ActionSequenceNode<'ActionSpace, 'Response> = {
     /// Links previous decision.
-    Previous: ContentId // ActionSequenceHead
+    Previous: ContentId<ActionSequenceHead>
 
     /// Holds timestamps and information obtained through the broker.
-    Result: ActionExecutionResult
+    Result: ActionExecutionResult<'ActionSpace, 'Response>
 }
 
 // ###################
@@ -107,46 +89,44 @@ and ActionSequenceNode = {
 // - sequence on different timestamps (observer or sequencer), or
 // - handle late arriving data differently (drop or rewind, up to a threshold)
 
-type Happening =
-    | Observation of CapturedObservation
-    | Action of ActionExecutionResult
+type Happening<'ObservationSpace, 'ActionSpace, 'Response> =
+    | Observation of CapturedObservation<'ObservationSpace>
+    | Action of ActionExecutionResult<'ActionSpace, 'Response>
 
-type ContextSequenceHead =
+type ContextSequenceHead<'ObservationSpace, 'ActionSpace, 'Response> =
     | Identity of Nonce:uint64
-    | Context of Node:ContextSequenceNode
-and ContextSequenceNode = {
+    | Context of Node:ContextSequenceNode<'ObservationSpace, 'ActionSpace, 'Response>
+and ContextSequenceNode<'ObservationSpace, 'ActionSpace, 'Response> = {
     /// Links previous context sequence head
-    Previous: ContentId // ContextSequenceHead
+    Previous: ContentId<ContextSequenceHead<'ObservationSpace, 'ActionSpace, 'Response>>
 
     /// The latest happening of this context sequence.
-    Happening: Happening
+    Happening: Happening<'ObservationSpace, 'ActionSpace, 'Response>
 }
 
 // ####################
 // ####  Strategy  ####
 // ####################
-//
-// Strategies are encoded roles that analyze information from a context sequence
 
 /// Decision sequence for strategy executions along a context sequence, where
 /// decisions are made from now into the future using the same strategy.
 /// It is produced by the runtime modules 'Live Strategy' and 'Backtester',
 /// with decision sequences of the live strategy module possibly being routed to an according 'Broker Group'.
-type DecisionSequenceHead =
-    | Start of DecisionSequenceStart
-    | Decision of DecisionSequenceNode
-and DecisionSequenceStart = {
+type DecisionSequenceHead<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response> =
+    | Start of DecisionSequenceStart<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response>
+    | Decision of DecisionSequenceNode<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response>
+and DecisionSequenceStart<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response> = {
     /// References the strategy to be used for every decision to be a valid decision sequence.
-    Strategy:ContentId // Sdk.Strategy
+    Strategy: ContentId<Sdk.Strategy<'ObservationSpace, 'QuerySurface, 'ActionSpace>>
 
     /// The first context the strategy should produce decisions on, with all later contexts being
     /// part of the same decision sequence for this to be a valid decision sequence.
-    FirstContext:ContentId // ContextSequenceHead
+    FirstContext: ContentId<ContextSequenceHead<'ObservationSpace, 'ActionSpace, 'Response>>
 }
-and DecisionSequenceNode = {
+and DecisionSequenceNode<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response> = {
     /// Links previous decision head of this decision sequence.
-    Previous: ContentId // DecisionSequenceHead
+    Previous: ContentId<DecisionSequenceHead<'ObservationSpace, 'QuerySurface, 'ActionSpace, 'Response>>
 
     /// What actions have been decided on by the evaluated strategy.
-    ActionSet: ContentId // ActionSet
+    Decision: ContentId<Sdk.Decision<'ActionSpace>>
 }

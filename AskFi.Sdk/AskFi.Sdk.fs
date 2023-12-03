aac3acc22f50c8e88d2a9d402442a3e32bdd0116
Bruno Zell <ask@bruno.wtf>
2023-12-03 17:22:31 +0100

    model percepts and actions

--- AskFi.Sdk/AskFi.Sdk.fs
+++ AskFi.Sdk/AskFi.Sdk.fs
@@ -8,26 +8,31 @@ open System.Threading.Tasks
 // #### OBSERVATIONS ####
 // ######################
 
-/// An atomic appearance of sensory information from within an IObserver<'Percept>.
-/// Includes a Percept, which is a typed (via a domain model) representation of the newly observed information.
-/// An observation can have multiple Percepts in case they all appeared at the same instant (point in time),
-/// with each Percept representing a single piece of sensory information each with a single 'Observation type selected from 'ObservationSpace.
-/// An individual observation, by definition, appeared at a singular instant (point in time), and must've originated from some IObserver<'Observation>.
-/// Since the 'Percept here is already strongly typed, it is data resulted from already interpreting the observed system.
-[<IsReadOnly; Struct>]
-type Observation<'ObservationSpace> = {
-    Percepts: 'ObservationSpace array
+/// The instance of a single observation from the given 'ObservationSpace.
+type Percept<'ObservationSpace> = {
+    SensoryInformation: specific_observation_of<'ObservationSpace>
 }
 
-/// Implemented by domain modules. An implementation defines imperative networking behavior to interact with
+/// An atomic appearance of sensory information from within an IObserver<'Percept>.
+/// Includes one or more Percepts, which is a typed (via a domain model) representation of the newly observed sensory information.
+/// An observation can have multiple Percepts in case they all appeared at the same instant (point in time),
+/// with each Percept representing a single piece of sensory information, each with a single 'Observation type selected from 'ObservationSpace.
+/// An individual observation, by definition, appeared at a singular instant (point in time), and must've originated from some IObserver<'Observation>.
+/// The 'Percept is strongly typed here, reflecting the IObserver's interpretation of the observed data.
+[<IsReadOnly; Struct>]
+type Observation<'ObservationSpace> = {
+    Percepts: Percept<'ObservationSpace> array
+}
+
+/// Implemented by domain modules. An IObserver implementation defines imperative networking behavior to interact with
 /// an external system with the aim of observing it to later infer what was happening. The observer implementation
 /// interprets the networking traffic and emits strongly-typed 'Percepts whenever there are new measurements obtained.
-type IObserver<'ObservationSpae> =
-    abstract member Observations : IAsyncEnumerable<Observation<'ObservationSpae>>
+type IObserver<'ObservationSpace> =
+    abstract member Observations : IAsyncEnumerable<Observation<'ObservationSpace>>
 
-// ###############
-// #### QUERY ####
-// ###############
+// #################
+// #### QUERIES ####
+// #################
 
 /// A measurement is an atomic appearance of sensory information for callers of IContextQueries,
 /// originating from captured observations of a single IObserver<'Observation> instance.
@@ -36,7 +41,7 @@ type IObserver<'ObservationSpae> =
 [<IsReadOnly; Struct>]
 type Measurement<'ObservationSpace> = {
     /// Sensory information captured by the producing IObserver.
-    observation: Observation<specific_observation_of<'ObservationSpace>>
+    observation: Observation<'ObservationSpace>
 
     /// Runtime timestamp at which this observation was produced by the IObserver.
     At: DateTime
@@ -57,13 +62,42 @@ type Context =
     abstract member inTimeRange<'ObservationSpace> : from: DateTime * ``to``: DateTime -> Measurement<'ObservationSpace> seq
 
 /// A query really is just a parameterized transformer mapping a context to an instance of some type 'Result.
-type Query<'Query, 'Result> =
+type Query<'ObservationSpace, 'Query, 'Result> =
     'Query -> Context -> 'Result
+
+// ###################
+// #### DECISIONS ####
+// ###################
+
+/// Virtually represents a specific abstract action with some 'Action type from 'ActionSpace
+type Action<'ActionSpace> = {
+    Action: specific_action_of<'ActionSpace>
+}
+
+/// Represents the strategies intent to execute one or more specified actions immediately.
+/// Where "immediately" is analogous to "now" in the evaluated context.
+/// With "now" being the contexts latest instant (point in time).
+/// With the contexts latest instant (point in time) being the latest observation in the context sequence.
+type Initiative<'ActionSpace> = {
+    Actions: Action<'ActionSpace> array
+}
+
+/// Represents the complete decision of a strategy when evaluated against a context.
+/// It can either decide to do nothing (Inaction),
+/// or decide to initiate one or more actions from the strategies 'ActionSpace.
+/// An 'ActionSpace is a discriminated union over one or more 'Action types,
+/// allowing the strategy to decide on multiple different 'Actions simultaneously,
+/// with each Initiative<'Action> selecting exactly one 'Action from that 'ActionSpace
+/// which then can be executed by an according IBroker<'Action, _>.
+type Decision<'ActionSpace> =
+    | Inaction
+    | Initiate of Initiative<'ActionSpace>
 
 // ##################
 // #### STRATEGY ####
 // ##################
 
+/// Represents a strategies past decision of previous points in time on the evaluated context.
 type DecisionReflection<'ActionSpace> = {
     /// The decision that is reflected on
     Decision: Decision<'ActionSpace>
@@ -91,25 +125,6 @@ type Reflection =
     /// Get an iterator the all Observations of type `'Perception` since the passed `from` until `to`
     /// (as determined by the runtime clock used during context sequencing).
     abstract member inTimeRange<'ActionSpace> : from: DateTime * ``to``: DateTime -> DecisionReflection<'ActionSpace> seq
-
-/// Represents the strategies intent to execute the specified action immediately
-// Where "immediately" is analogous to "now" in the evaluated context.
-/// With "now" being the contexts latest instant (point in time).
-/// With the contexts latest instant (point in time) being the latest observation in the context sequence.
-type Initiative<'Action> = {
-    Action: 'Action
-}
-
-/// Represents the complete decision of a strategy when evaluated against a context.
-/// It can either decide to do nothing (Inaction),
-/// or decide to initiate one or more actions from the strategies 'ActionSpace.
-/// An 'ActionSpace is a discriminated union over one or more 'Action types,
-/// allowing the strategy to decide on multiple different 'Actions simultaneously,
-/// with each Initiative<'Action> selecting exactly one 'Action from that 'ActionSpace
-/// which then can be executed by an according IBroker<'Action, _>.
-type Decision<'ActionSpace> =
-    | Inaction
-    | Initiate of Initiatives:Initiative<specific_action_of<'ActionSpace>> array
 
 /// Contains the code of a strategy decision, called upon each evolution of the Askbot sessions context (i.e. on every new observation).
 type Strategy<'ObservationSpace, 'ActionSpace> =

be52dcc3b2287e6f7f74eea7dd96f1b43a4166f2
Bruno Zell <ask@bruno.wtf>
2024-03-20 19:50:52 +0100

    protocol ses

--- Ask.DataModel/Ask.DataModel2.fs
+++ Ask.DataModel/Ask.DataModel2.fs
@@ -3,8 +3,14 @@ module Ask.DataModel
 open System
 open Ask.Host.Persistence
 
-/// Generated on every network message sent or received from within an IObserver implementation.
-/// 'Message is a Protocols Message-type
+/// Unique identifier for a single IO-Instance (Observer-Instance or Broker-Instance)
+/// from controller behavior new network protocol sessions are created.
+type InteractorIdentity =
+    | Observer of StartTimestamp:DateTime * Nonce:int64
+    | Broker of StartTimestamp:DateTime * Nonce:int64
+
+/// Generated on every network message sent or received from within an IObserver or IBroker implementation.
+/// 'Message is a network protocols 'Message-type
 type CapturedMessage<'Message> = {
     /// Absolute timestamp of when this observation was recorded.
     /// As of runtime clock.
@@ -14,12 +20,16 @@ type CapturedMessage<'Message> = {
     Message: ContentId<'Message>
 }
 
-/// All captured observations within an observer group are sequenced into
-/// an observation sequence. Isolated observation sequences are a form of
-/// entry point for new information into the system. CIDs to such sequences
-/// are passed around to share information.
+/// All network sessions originating from an IO-Instances (IObserver and IBroker instances)
+/// are recorded into protocol session sequences.
+/// These sequences reference the origin IO-Instance in it's first node and sequence all network
+/// message exchanged through that network session.
+/// What constitutes a session is defined by each protocol implementation separately.
+/// Network sessions with external IT-Systems are the only entry point for new information into the system.
+/// Special network protocol clients are used in IObserver and IBroker implementations to implicitly capture
+/// all network messages sent and received.
 type ProtocolSessionHead<'Message> =
-    | Identity of Nonce:uint64
+    | Identity of Origin:InteractorIdentity * InitiationTimestamp:DateTime * Nonce:int64
     | Message of Node:ProtocolSessionMessage<'Message>
 and ProtocolSessionMessage<'Message> = {
     /// Links previous ProtocolSessionHead to form a temporal order.

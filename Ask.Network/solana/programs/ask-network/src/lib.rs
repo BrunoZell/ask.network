6d4d9f990a39dabe2b63195a9b88b5ca41d64a37
Bruno Zell <ask@bruno.wtf>
2024-03-22 15:43:29 +0100

    use user login

--- Ask.Network/solana/programs/ask-network/src/lib.rs
+++ Ask.Network/solana/programs/ask-network/src/lib.rs
@@ -61,14 +61,14 @@ pub mod ask_network {
 pub struct InitializeUser<'info> {
     #[account(
         init,
-        seeds= [user.key().as_ref()],
+        seeds= [user_login.key().as_ref()],
         bump,
         space = 8 + 8 + 8,
-        payer = user)]
+        payer = user_login)]
     pub user_account: Account<'info, User>,
 
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub user_login: Signer<'info>,
 
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
@@ -83,7 +83,7 @@ pub struct InitializeUser<'info> {
 pub struct PlaceAsk<'info> {
     #[account(
         init, // this 'ask' account will be initialized
-        seeds = [user.key().as_ref(), &user_account.running_ask_ordinal.to_le_bytes()], // unique ask address from user key and ordinal
+        seeds = [user_account.key().as_ref(), &user_account.running_ask_ordinal.to_le_bytes()], // unique ask address from user key and ordinal
         bump,
         payer = user, // 'user' account pays fees
         space = 8 + 4 + content.len() + 8)]
@@ -91,12 +91,12 @@ pub struct PlaceAsk<'info> {
 
     #[account(
         mut, // users running ask ordinal is incremented after ask placement
-        seeds = [user.key().as_ref()], // 'user' account is derived from the users public key
+        seeds = [user_login.key().as_ref()], // 'user' account is derived from the users public key
         bump)]
     pub user_account: Account<'info, User>,
-    
+
     #[account(mut)]
-    pub user: Signer<'info>, // signer of the transaction, implying the 'user' account
+    pub user_login: Signer<'info>, // signer of the transaction, implying the 'user_login' account
 
     // Solana's built-in system program. Required for operations like account initialization.
     pub system_program: Program<'info, System>,
@@ -107,20 +107,20 @@ pub struct PlaceAsk<'info> {
 pub struct UpdateAsk<'info> {
     #[account(
         mut, // the content of the existing 'ask' account will be mutated
-        seeds = [user.key().as_ref(), &ordinal.to_le_bytes()], // 'ask' account is identified by instruction parameters
+        seeds = [user_account.key().as_ref(), &ordinal.to_le_bytes()], // 'ask' account is identified by instruction parameters
         bump,
         realloc = 8 + 4 + content.len() + 8 ,
         realloc::zero = true,
-        realloc::payer = user)] // 'user' account pays fees
+        realloc::payer = user_account)] // 'user' account pays fees
     pub ask: Account<'info, Ask>,
-    
+
     #[account(
-        seeds = [user.key().as_ref()],
+        seeds = [user_login.key().as_ref()],
         bump)]
     pub user_account: Account<'info, User>,
-    
+
     #[account(mut)]
-    pub user: Signer<'info>, // signer of the transaction, implying the 'user' account
+    pub user_login: Signer<'info>, // signer of the transaction, implying the 'user_login' account
 
     pub system_program: Program<'info, System>,
 }
@@ -129,19 +129,19 @@ pub struct UpdateAsk<'info> {
 #[instruction(ordinal: u64)]
 pub struct CancelAsk<'info> {
     #[account(
-        mut, 
-        close = user, // after the instruction is executed, the 'ask' account will be closed, and any remaining lamports will be transferred to the 'user' account.
-        seeds = [user.key().as_ref(), &ordinal.to_le_bytes()],
+        mut,
+        close = user_login, // after the instruction is executed, the 'ask' account will be closed, and any remaining lamports will be transferred to the 'user' account.
+        seeds = [user_account.key().as_ref(), &ordinal.to_le_bytes()],
         bump)]
     pub ask: Account<'info, Ask>,
 
-    #[account(mut)]
-    pub user: Signer<'info>,
-
     #[account(
-        seeds = [user.key().as_ref()],
+        seeds = [user_login.key().as_ref()],
         bump)]
     pub user_account: Account<'info, User>,
+
+    #[account(mut)]
+    pub user_login: Signer<'info>,
 
     pub system_program: Program<'info, System>,
 }

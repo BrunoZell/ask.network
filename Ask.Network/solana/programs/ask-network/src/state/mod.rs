c28b617aba64e4a2b90f34aa593d6338530c5565
Bruno Zell <ask@bruno.wtf>
2024-03-29 17:45:48 +0000

    separate file for germany

--- Ask.Network/solana/programs/ask-network/src/state/mod.rs
+++ Ask.Network/solana/programs/ask-network/src/state/mod.rs
@@ -1,10 +1,14 @@
 pub use ask::*;
+pub use commercial_register::*;
+pub use commercial_register_germany::*;
 pub use global::*;
 pub use membership::*;
 pub use organization::*;
 pub use user::*;
 
 mod ask;
+mod commercial_register;
+mod commercial_register_germany;
 mod global;
 mod membership;
 mod organization;

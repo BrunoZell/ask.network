4ce0e7a6ecb38b10c908b2d387573864737d8f8a
Bruno Zell <ask@bruno.wtf>
2024-04-07 18:13:03 +0000

    rm using

--- Ask.Network/frontend/pages/index.tsx
+++ Ask.Network/frontend/pages/index.tsx
@@ -1,23 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Box, Button, Flex, flexbox, Input, Stack, VStack } from '@chakra-ui/react';
-import * as anchor from '@project-serum/anchor';
-import {
-  useAnchorWallet,
-  useConnection,
-  useWallet,
-} from '@solana/wallet-adapter-react';
-import idl from '../../solana/target/idl/ask_network.json';
-import { PublicKey } from '@solana/web3.js';
-import { AskNetwork as AskIdl } from '../../solana/target/types/ask_network';
-import { IdlAccounts, Program } from '@project-serum/anchor';
 import { AppBar } from '../components/AppBar';
-import { createCloseAccountInstruction, getAccount, getAssociatedTokenAddress } from '@solana/spl-token';
-import {
-  IdlAccount,
-  IdlEnumFields,
-  IdlEnumVariant,
-} from '@project-serum/anchor/dist/cjs/idl';
-
 
 const HomePage = () => {
   return (
